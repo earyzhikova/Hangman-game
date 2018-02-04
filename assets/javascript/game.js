6bb057ae1914c7ee6d3a89bf44a377fd13d091d7
earyzhikova <elenaryzh55@gmail.com>
2018-02-04 10:07:53 -0500

    hangman-game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,133 +1,167 @@
-    // an array with all the options
-    
-
-    // user presses a key.
-    document.onkeyup = function(event) {
-
-      // which key was pressed.
-      var userGuess = event.key;
-
-//
-
-var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var wordBank = ["word1", "word2", "word3"];
+var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var wordBank = ["Everest", "Denali", "Kilimanjaro", "Aconcagua", "Elcapitan", "Whiteface", "Montblanc", "Grandfather", "Mitchell"];
 var wins = 0;
 var losses = 0;
-var guessLeft = 10;
-var yourGuess = [];
+var numberChances = 6;
+var wrongGuess = [];
+var randomWord = "";
+
+document.addEventListener("DOMContentLoaded", function() {
+ setUp()
+}, false);
 
 
-function isGuessCorrect(guess) {
-  if () {
-    return true;
-  } else {
-    return false;
+function setUp (){
+wrongGuess = [];
+numberChances = 6;
+randomWord = wordBank[Math.floor(Math.random()*wordBank.length)];
+var underscoreText = "";
+for(var i = 0; i < randomWord.length; i ++){
+  underscoreText += " _";
+}
+
+document.getElementById("guessPanel").innerHTML = underscoreText; 
+}
+
+
+ // user presses a key.
+document.onkeypress = function(event) {
+      var userGuess = event.key;
+
+      isInAlphabet(userGuess);
+    }
+// make sure user guess was in alphabet
+
+function isInAlphabet(userGuess) {
+  for(var i = 0; i < alphabet.length; i++){
+    if ((userGuess  == alphabet[i].toLowerCase()) ||  (userGuess  == alphabet[i].toUpperCase())) {
+    }
+
+  }
+
+}
+
+//correct guess
+function isGuessCorrect(userGuess) {
+    if (randomWord.charAt(0)) {
+    for (var i = 0; i < randomWord.length; i++){
+
+// }
+    }
   }
 }
+// to replace underscore with correctly guessed letter
 
-function replaceUnderscore(guess) {
-  // to replace underscore with correctly guessed letter
-}
+function replaceUnderscore(userGuess) {
+  for(var i = 0; i < randomWord.length; i++){
+   if (this.wordBank.charAt(i) === guess) {
+        this.randomWord[i] = guess;
 
-function increaseWins() {
-  // to increase win count after user wins
-}
-
-function increaseLosses() {
-  // to increase loss count after user loses
-}
-
-function addToWrongLetterArray(guess) {
-  // to add incorrect letter guess to yourGuess[]
-}
-
-function isInAlphabet(guess) {
-  // to check if user guess is valid guess (in alphabet)
-}
-
-function isGuessValid(guess) {
-  // make sure user guess wasn't already guessed (1- your guess array 2- hangman word)
-}
-
-
-if (alphabet.indexOf(guess) !== -1) {
-
-}
-
-function hangmanGame(guess) {
-
-  // make sure user guess was in alphabet
-isLetters(arr) { for (var i = 0; i < arr.length; i++) { if(arr[i].toLowerCase() != arr[i].toUpperCase()){ } else { return false; } } return true; }
-
-  // test if guess was correct
-      // if correct, show letter
-      // if incorrect, decrement 'guessLeft', your guesses array
-
-
-  // test if user won the game
-      // increase win #
-      // call function to reset game called resetRound()
-
-
-  // test if user lost the game
-      // increase loss #
-      // call function to reset game called resetRound()
-
-
-  // reset round
-
+      } 
+    }
 }
 
 
 
 
+// function updateGuesses(userGuess) {
+//   numberChances--; // decrement guesses left
+//   letterCountElement.innerHTML = numberChances;
+//   // letter is not in the word
+//   // update letters guessed
+//   if (randomWord.indexOf(letter) === -1) { 
+//     wrongGuesses.push(letter); 
+//     lettersGuessedElement.innerHTML = wrongGuess.join(', ');
+
+//   }
+// // letter is in the word
+//   // replace underscore with the letter
+//   else { 
+//     for (var i = 0; i < randomWord.length; i++) {
+//       if (randomWord[i] === letter) {
+//         correctGuesses[i] = letter;
+//       }
+//     }
+// }
+// }
+
+  
+// updateHangmanWord(guess) {
+
+//     // Iterate over hangman word
+//     for (let i = 0; i < this.word.length; i++) {
+
+//       // Replace underscore with letter if guess is a match
+//       if (this.word.charAt(i) === guess) {
+//         this._hangmanWord[i] = guess;
+//       }
+//     }
+//   },
+// // function increaseWins() {
+// //   // to increase win count after user wins
+// // }
+
+// function increaseLosses() {
+//   // to increase loss count after user loses
+// }
+
+// function addToWrongLetterArray(guess) {
+//   // to add incorrect letter guess to yourGuess[]
+// }
 
 
-document.onkeyup = function(event) {
 
-  var userGuess = event.key;
+// function isGuessValid(guess) {
+//   // make sure user guess wasn't already guessed (1- your guess array 2- hangman word)
+// }
 
-  hangmanGame(userGuess);
 
-}
+// if (alphabet.indexOf(guess) !== -1) {
 
-//
-      // Randomly chooses a choice from the options array. This is the Computer's guess.
-      var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+// }
 
-      
-      // This logic determines the outcome of the game (win/loss/left guesses/ desplayed guesses), and increments the appropriate number
-      if ((userGuess === "#") || (computerGuess === "#") {
+// function hangmanGame(guess) {
 
-        if ((userGuess === "0") && (computerGuess === "0")) {
-          wins++;
-        } else if ((userGuess === "r") && (computerGuess === "p")) {
-          losses++;
-        } else if ((userGuess === "s") && (computerGuess === "r")) {
-          losses++;
-        } else if ((userGuess === "s") && (computerGuess === "p")) {
-          wins++;
-        } else if ((userGuess === "p") && (computerGuess === "r")) {
-          wins++;
-        } else if ((userGuess === "p") && (computerGuess === "s")) {
-          losses++;
-        } else if (userGuess === computerGuess) {
-          ties++;
-        }
 
-        // Creating a variable to hold our new HTML. Our HTML now keeps track of the user and computer guesses, and wins/losses/ties.
-        var html =
-          "<p>You chose: " + userGuess + "</p>" +
-          "<p>The computer chose: " + computerGuess + "</p>" +
-          "<p>wins: " + wins + "</p>" +
-          "<p>losses: " + losses + "</p>" +
-          "<p>ties: " + ties + "</p>";
+//   // test if guess was correct
+//       // if correct, show letter
+//       // if incorrect, decrement 'guessLeft', your guesses array
 
-        // NOTE: innerHTML works like textContent except that it also lets us use HTML tags in the string (textContent
-        // doesn't allow HTML tags).
 
-        // Set the inner HTML contents of the #game div to our html string
-        userText.innerHTML = html;
-      }
-    };
-  </script>
+//   // test if user won the game
+//       // increase win #
+//       // call function to reset game called resetRound()
+
+
+//   // test if user lost the game
+//       // increase loss #
+//       // call function to reset game called resetRound()
+
+
+//   // reset round
+
+// }
+
+
+
+
+
+
+// document.onkeyup = function(event) {
+
+//   var userGuess = event.key;
+
+//   hangmanGame(userGuess);
+
+// }
+
+// //
+     
+
+       
+
+//         // Set the inner HTML contents of the #game div to our html string
+//         userText.innerHTML = html;
+//       }
+//     };
+//   </script>
