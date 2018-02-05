cb4a59da1768655a5118b4c1658dbb32273e2181
earyzhikova <elenaryzh55@gmail.com>
2018-02-05 15:17:30 -0500

    Hangman-game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,80 +7,89 @@
   var randomWord = "";
 
 
-
-  document.addEventListener("DOMContentLoaded", function() {
-   setUp()
+  /**
+   * Runs when the page is loaded
+   */
+  document.addEventListener("DOMContentLoaded", function () {
+      setUp()
   }, false);
 
 
+  /**
+   * Does game setup/reset
+   */
   function setUp() {
-  wrongGuess = [];
-  guessRemain = 6;
-  randomWord = wordBank[Math.floor(Math.random()*wordBank.length)];
-  var underscoreText = "";
-  for(var i = 0; i < randomWord.length; i ++){
-    underscoreText += " _";
-      console.log(randomWord);
-
-    }
+      wrongGuess = [];
+      guessRemain = 6;
+      randomWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+      var underscoreText = "";
+      for (var i = 0; i < randomWord.length; i++) {
+          underscoreText += " _";
+          document.getElementById('guessPanel').innerHTML = underscoreText;
+          console.log(randomWord);
+      }
   }
 
-  console.log(randomWord);
-  document.getElementById("guessPanel").innerHTML = underscoreText; 
+function replaceAt(string, index, replace) {
+  return string.substring(0, index) + replace + string.substring(index + 1);
+}
 
-
-
-   // user presses a key.
-  document.onkeypress = function(event) {
-        var userGuess = event.key;
-        console.log(userGuess);
-        isInAlphabet(userGuess);
-       var correctLetterNumbers =  isGuessCorrect(userGuess);
-       if (correctLetterNumbers !== []) {
-          for (var i = 0; correctLetterNumbers.length > i; i++) {
-            document.getElementById(guessPanel).innerHTML = underscoreText; 
-            underscoreText =  document.getElementById("guessPanel") = correctLetterNumbers;
-            console.log(underscoreText);
-         //   underscoreText.charAt(correctLetterNumbers[i]*2) = userGuess;
-
+  /**
+   * Runs when a key is pressed
+   * @param {*A key is pressed} event 
+   */
+  document.onkeypress = function (event) {
+      var userGuess = event.key;
+      console.log(userGuess);
+      if(isInAlphabet(userGuess)) {
+        var correctLetterNumbers = isGuessCorrect(userGuess);
+        var underscoreText = '';
+        if (correctLetterNumbers.length != 0) {
+          //replacing underscores
+            for (var i = 0; correctLetterNumbers.length > i; i++) {
+                underscoreText = document.getElementById('guessPanel').innerHTML;
+                underscoreText = replaceAt(underscoreText, correctLetterNumbers[i]*2, userGuess);
+                document.getElementById("guessPanel").innerHTML = underscoreText;
+            }
+            //checking if they won the game (no underscores)
+          if (underscoreText.indexOf(' _') == -1){
+            var winText = document.getElementById('js-wins').innerHTML;
+            winText = parseInt(winText)+1;
+            document.getElementById('js-wins').innerHTML = winText;
+            alert('Won Game!');
+            setUp();
           }
-       }
-       else {
-        wrongGuess.push(i); 
-      wrongGuess.getElementById.innerHTML = wrongGuess.join(', ');
+        }
+        else {
+            var guessRemain = document.getElementById("js-guess-remain").innerHTML;
+            guessRemain = parseInt(guessRemain)-1;
+            document.getElementById("js-guess-remain").innerHTML = guessRemain;
+            if(guessRemain < 1){
+              alert("Lost the Game!")
+            }
 
-  //   }
 
-       }
-
+            // wrongGuess.push(i);
+            // wrongGuess.getElementById.innerHTML = wrongGuess.join(', ');
+        }
       }
-  // make sure user guess was in alphabet
+  }
 
+
+  /**
+   * Checks if the user guess is in the alphabet
+   * @param {*The guess letter} userGuess 
+   */
   function isInAlphabet(userGuess) {
-    for(var i = 0; i < alphabet.length; i++) {
-      if ((userGuess  == alphabet[i].toLowerCase()) ||  (userGuess  == alphabet[i].toUpperCase())) {
-        return true;
-      } else {
-        return false;
+    var isIndeedInAlphabet = false;
+      for (var i = 0; i < alphabet.length; i++) {
+          if ((userGuess == alphabet[i].toLowerCase()) || (userGuess == alphabet[i].toUpperCase())) {
+             isIndeedInAlphabet = true;
+          }
       }
-
-    }
-
+    return isIndeedInAlphabet;
   }
 
-
-
-  function incrementwins() {
-   wins++;
-  }
-
-  function incrementlosses() {
-   losses++;
-  }
-
-  function decrementguessRemain() {
-    guessRemain--;
-  }
 
   function whenUserLose() {
       if (guessRemain === 0) {
@@ -98,16 +107,39 @@
       }
   }
 
-  function replaceUnderscore(userGuess) {
-    for (var i = 0; i < randomWord.length; i++) {
-      if (userGuess === randomWord[i]) {
-        underscoreText[i] = userGuess;
-      }
-    }
+  function isGuessCorrect(userGuess) {
+    var correctLetterNumbers = [];
+    //for random word length check each letter
+     for (var i = 0; i < randomWord.length; i++) {
+        if(( userGuess == randomWord.charAt(i).toLowerCase()) || (userGuess == randomWord.charAt(i).toUpperCase())){
+          correctLetterNumbers.push(i);
+        }
+     }
+     console.log(correctLetterNumbers);
+
+    return(correctLetterNumbers);
+
+    //   if (randomWord.indexOf(userGuess) !== -1) {
+    //       return true;
+    //       // call replaceUnderscore(userGuess)
+    //   } else {
+    //       return false;
+    //   }
   }
 
+  // function replaceUnderscore(userGuess) {
+  //     for (var i = 0; i < randomWord.length; i++) {
+  //         if (userGuess === randomWord[i]) {
+  //             underscoreText[i] = userGuess;
+  //         }
+  //     }
+  // }
 
-   // to replace underscore with correctly guessed letter
+
+  //   console.log(randomWord);
+  //   document.getElementById("guessPanel").innerHTML = underscoreText; 
+
+  // to replace underscore with correctly guessed letter
 
   // function replaceUnderscore(userGuess) {
   //   for(var i = 0; i < randomWord.length; i++){
@@ -119,139 +151,138 @@
   // }
 
 
-  function isGuessCorrect(userGuess) { 
-    if (randomWord.indexOf(userGuess) !== -1) {
-      return true;
-      // call replaceUnderscore(userGuess)
-    } else {
-      return false;
-    }
-  }
 
 
-
-  function newRound() {
-      guessRemain = 6;
-      wrongGuess = [];
-      chooserandomWord();
-      createunderscoreText();
-      updateBrowserWindow();
-  }
-
-  function updateBrowserWindow() {
-      document.getElementById('js-wins').innerHTML = wins;
-      document.getElementById('js-losses').innerHTML = losses;
-      document.getElementById('js-guess-remain').innerHTML = guessRemain;
-      document.getElementById('js-guessPanel').innerHTML = underscoreText;
-      document.getElementById('js-wrong-guess').innerHTML = wrongGuess;
-
-
-
-  function gameEngine(userGuess) {
-      
-      // is user guess a letter
-      if (isInAlphabet(userGuess)) {
-          
-          // if so, is user guess previously guessed
-          if (isGuessValid(userGuess)) {
-              
-              // is user guess correct
-              if (isGuessCorrect(userGuess)) {
-                  
-                  // replace underscore with user guess at proper indices
-                  replaceUnderscore(userGuess);
-                  updateBrowserWindow();
-              } else { // if user guess was wrong
-                  
-                  // add letter to letters guessed array
-                  addTowrongGuess(userGuess);
-                  decrementguessRemain();
-                  updateBrowserWindow();
-              }
-          } else { /* do nothing */ }
-      } else { /* do nothing */ }
-      
-      // Test if user won
-      if (whenUserWin()) {
-          incrementwins();
-          console.log("The Player Won This Round");
-          newRound();
-      }
-      
-      // Test if user lost
-      if (whenUserLose()) {
-          incrementlosses();
-          console.log("The Player Lost This Round");
-          newRound();
-      }
-  
-
-  
-newRound();
-
-document.addEventListener('keyup', function(event) {
-    userGuess = event.key.toUpperCase();
-    gameEngine(userGuess);
-
-   }); 
-    // 
-
-
-
-  //     var correctLetterNumbers = []   
-      // for (var i = 0; i < randomWord.length; i++)
-  //     {
-  //       // console.log(randomWord.charAt(i));
-  //       if ((userGuess  == randomWord[i].toLowerCase()) ||  (userGuess  == randomWord[i].toUpperCase())) {
-      
-  //       correctLetterNumbers.push(i);
-  //       alert(randomWord);
-  //     }
-  //    }
-    
-  //       return correctLetterNumbers;
-  // }
-
-
-
-   // function updateGuesses(userGuess) {
-  //   numberChances--; // decrement guesses left
-  //   letterCountElement.innerHTML = numberChances;
-  //   // letter is not in the word
-  //   // update letters guessed
-  //   if (randomWord.indexOf(letter) === -1) { 
-  //     wrongGuesses.push(letter); 
-  //     lettersGuessedElement.innerHTML = wrongGuess.join(', ');
-
+  //   function incrementwins() {
+  //    wins++;
   //   }
-  // // letter is in the word
-  //   // replace underscore with the letter
-  //   else { 
-  //     for (var i = 0; i < randomWord.length; i++) {
-  //       if (randomWord[i] === letter) {
-  //         correctGuesses[i] = letter;
+
+  //   function incrementlosses() {
+  //    losses++;
+  //   }
+
+  //   function decrementguessRemain() {
+  //     guessRemain--;
+  //   }
+
+  //   function newRound() {
+  //       guessRemain = 6;
+  //       wrongGuess = [];
+  //       chooserandomWord();
+  //       createunderscoreText();
+  //       updateBrowserWindow();
+  //   }
+
+  //   function updateBrowserWindow() {
+  //       document.getElementById('js-wins').innerHTML = wins;
+  //       document.getElementById('js-losses').innerHTML = losses;
+  //       document.getElementById('js-guess-remain').innerHTML = guessRemain;
+  //       document.getElementById('js-guessPanel').innerHTML = underscoreText;
+  //       document.getElementById('js-wrong-guess').innerHTML = wrongGuess;
+
+
+
+  //   function gameEngine(userGuess) {
+
+  //       // is user guess a letter
+  //       if (isInAlphabet(userGuess)) {
+
+  //           // if so, is user guess previously guessed
+  //           if (isGuessValid(userGuess)) {
+
+  //               // is user guess correct
+  //               if (isGuessCorrect(userGuess)) {
+
+  //                   // replace underscore with user guess at proper indices
+  //                   replaceUnderscore(userGuess);
+  //                   updateBrowserWindow();
+  //               } else { // if user guess was wrong
+
+  //                   // add letter to letters guessed array
+  //                   addTowrongGuess(userGuess);
+  //                   decrementguessRemain();
+  //                   updateBrowserWindow();
+  //               }
+  //           } else { /* do nothing */ }
+  //       } else { /* do nothing */ }
+
+  //       // Test if user won
+  //       if (whenUserWin()) {
+  //           incrementwins();
+  //           console.log("The Player Won This Round");
+  //           newRound();
   //       }
-  //     }
-  // }
-  // }
 
-    
-  // updateHangmanWord(guess) {
-
-  //     // Iterate over hangman word
-  //     for (let i = 0; i < this.word.length; i++) {
-
-  //       // Replace underscore with letter if guess is a match
-  //       if (this.word.charAt(i) === guess) {
-  //         this._hangmanWord[i] = guess;
-  //       }
-  //     }
-  //   },
-
-  // function isGuessValid(guess) {
-  //   // make sure user guess wasn't already guessed (1- your guess array 2- hangman word)
-  // }
+  //       // Test if user lost
+  //       if (whenUserLose()) {
+  //           incrementlosses();
+  //           console.log("The Player Lost This Round");
+  //           newRound();
+  //       }t
 
 
-  
-  
+
+  // newRound();
+
+  // document.addEventListener('keyup', function(event) {
+  //     userGuess = event.key.toUpperCase();
+  //     gameEngine(userGuess);
+
+  //    }); 
+  //     // 
+
+
+
+    //     var correctLetterNumbers = []   
+        // for (var i = 0; i < randomWord.length; i++)
+    //     {
+    //       // console.log(randomWord.charAt(i));
+    //       if ((userGuess  == randomWord[i].toLowerCase()) ||  (userGuess  == randomWord[i].toUpperCase())) {
+
+    //       correctLetterNumbers.push(i);
+    //       alert(randomWord);
+    //     }
+    //    }
+
+    //       return correctLetterNumbers;
+    // }
+
+
+
+     // function updateGuesses(userGuess) {
+    //   numberChances--; // decrement guesses left
+    //   letterCountElement.innerHTML = numberChances;
+    //   // letter is not in the word
+    //   // update letters guessed
+    //   if (randomWord.indexOf(letter) === -1) { 
+    //     wrongGuesses.push(letter); 
+    //     lettersGuessedElement.innerHTML = wrongGuess.join(', ');
+
+    //   }
+    // // letter is in the word
+    //   // replace underscore with the letter
+    //   else { 
+    //     for (var i = 0; i < randomWord.length; i++) {
+    //       if (randomWord[i] === letter) {
+    //         correctGuesses[i] = letter;
+    //       }
+    //     }
+    // }
+    // }
+
+
+    // updateHangmanWord(guess) {
+
+    //     // Iterate over hangman word
+    //     for (let i = 0; i < this.word.length; i++) {
+
+    //       // Replace underscore with letter if guess is a match
+    //       if (this.word.charAt(i) === guess) {
+    //         this._hangmanWord[i] = guess;
+    //       }
+    //     }
+    //   },
+
+    // function isGuessValid(guess) {
+    //   // make sure user guess wasn't already guessed (1- your guess array 2- hangman word)
+    // }
